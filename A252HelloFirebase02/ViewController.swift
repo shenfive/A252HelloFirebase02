f21a022dfc5d291d86952d1381c5858dcf47a652
Danny Shen <shenfive@gmail.com>
2025-03-23 09:37:51 +0800

    Initial Commit

---
+++ A252HelloFirebase02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A252HelloFirebase02
+//
+//  Created by 申潤五 on 2025/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
